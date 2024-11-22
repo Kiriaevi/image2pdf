e9b877ca80fe0f58c8cd2cfa70f8d832942b0559
Francesco <91418890+Kiriaevi@users.noreply.github.com>
2024-11-22 20:37:16 +0100

    Iniziata a scrivere funzionalità per scattare le foto

--- app/src/main/java/com/example/image2pdf/Fotocamera.kt
+++ app/src/main/java/com/example/image2pdf/Fotocamera.kt
@@ -2,14 +2,17 @@ package com.example.image2pdf
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import android.Manifest
+import android.content.ContentValues
 import android.content.pm.PackageManager
 import android.os.Build
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.video.Recorder
@@ -18,6 +21,8 @@ import androidx.camera.video.VideoCapture
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.image2pdf.databinding.ActivityFotocameraBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
 import java.util.concurrent.ExecutorService
 
 // https://developer.android.com/codelabs/camerax-getting-started#1
@@ -46,7 +51,6 @@ class Fotocamera : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityFotocameraBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-
         // Richiedi i permessi per la fotocamera
         if (allPermissionsGranted()) {
             startCamera()
@@ -55,8 +59,6 @@ class Fotocamera : AppCompatActivity() {
         }
         enableEdgeToEdge()
         //setContentView(R.layout.activity_fotocamera)
-
-
     }
     private val activityResultLauncher =
         registerForActivityResult(
@@ -77,13 +79,58 @@ class Fotocamera : AppCompatActivity() {
             }
         }
 
-    private fun takePhoto() {}
+    private fun takePhoto() {
+        // usa l'istanza di imageCapture se definita, se null allora fai un return
+        // senza il return l'applicazione crasha
+        // https://developer.android.com/reference/kotlin/androidx/camera/core/ImageCapture
+        val imageCapture = imageCapture ?: return
+
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.ITALY)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Toast.makeText(baseContext,
+                        "Cattura foto fallita",
+                        Toast.LENGTH_SHORT).show()
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun
+                        onImageSaved(output: ImageCapture.OutputFileResults){
+                    Toast.makeText(baseContext,
+                        "Cattura foto riuscita",
+                        Toast.LENGTH_SHORT).show()
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            }
+        )
+    }
 
     private fun captureVideo() {}
-
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
         // il primo parametro di addListener è un Runnable, il secondo è un Excecutor
         // In particolare l'executor è quello del main thread, mentre il runnable lo definiamo
         // noi con le parentesi {} e chiediamo di impostare il nostro object Preview con le
@@ -99,6 +146,10 @@ class Fotocamera : AppCompatActivity() {
                     it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
                 }
 
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             try {
@@ -107,16 +158,15 @@ class Fotocamera : AppCompatActivity() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
+                    this, cameraSelector, preview, imageCapture)
 
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
 
         }, ContextCompat.getMainExecutor(this))
-
+        Log.d(TAG, "Fotocamera avviata!")
     }
-
     private fun requestPermissions() {
         activityResultLauncher.launch(REQUIRED_PERMISSIONS)
     }
@@ -131,8 +181,9 @@ class Fotocamera : AppCompatActivity() {
     }
 
     companion object {
-        private const val TAG = "CameraXApp"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val TAG = "FOTOCAMERAX"
+        // ci serve nella funzione takePhoto(), per  salvare le immagini con un timestamp
+        private const val FILENAME_FORMAT = "dd-MM-yyyy-HH-mm-ss-SSS"
         private val REQUIRED_PERMISSIONS =
             mutableListOf (
                 Manifest.permission.CAMERA,
