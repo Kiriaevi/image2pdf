7310533ac8a86b93f8a0dce8dff79585b4f5b13b
aetoropc <cristiantrofusha1@gmail.com>
2024-11-23 15:08:36 +0100

    -MainActivity nel manifest cambiata -Aggiunte activity nuovoPdf e condivisione per iniziare a struttura l'app ad albero -Ho provato a creare un tema per cambiare i colori orribili, ma la main activity non intende rispondermi

--- app/src/main/java/com/example/image2pdf/MainActivity.kt
+++ app/src/main/java/com/example/image2pdf/MainActivity.kt
@@ -1,13 +1,38 @@
 package com.example.image2pdf
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        attendiEvento()
     }
+
+    fun attendiEvento(){
+        val bottone1 = findViewById<Button>(R.id.bottone1)
+        val bottone2 = findViewById<Button>(R.id.bottone2)
+        bottone1.setOnClickListener {
+            cambiaActivity(NuovoPdf::class.java)
+        }
+        bottone2.setOnClickListener {
+            cambiaActivity(Condivisione::class.java)
+        }
+    }
+
+    fun cambiaActivity(classe :Class<out Activity>){
+        val intent = Intent(this,classe)
+        startActivity(intent)
+    }
+
 }
