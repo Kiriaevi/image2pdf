10af38ccf7b6fe02b9cd683692ec28cdac73de00
aetoropc <cristiantrofusha1@gmail.com>
2024-11-25 19:08:35 +0100

    -Aggiunto metodo che salva le immaggini scelte per creare il pdf (Necessario ancora il passaggio al creaPdf)

--- app/src/main/java/com/example/image2pdf/Condivisione.kt
+++ app/src/main/java/com/example/image2pdf/Condivisione.kt
@@ -3,6 +3,8 @@ package com.example.image2pdf
 import android.app.Activity
 import android.content.ClipData
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
@@ -11,10 +13,18 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.collection.emptyLongSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.itextpdf.commons.utils.Base64.InputStream
 
 class Condivisione : AppCompatActivity() {
+
+
+    companion object {
+        private var arrayOfBitmap = mutableListOf<Bitmap>()//Raccoglitore di foto di famiglia
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -33,14 +43,36 @@ class Condivisione : AppCompatActivity() {
             intentIm.type="image/*"
             intentIm.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true)
             startActivityForResult(intentIm,1)
-            val foto : ClipData? = intentIm.clipData //Il ? serve per indicare la possibilità di "Non ritorno"
-            //Se tutto va bene qui dovrei avere le foto e dopodicchè chiamare la funzione per generare il pdf
-            //Non ho capito allora a che serve il request code
         }
     }
 
     fun cambiaActivity(classe :Class<out Activity>){
         val intent = Intent(this,classe)
         startActivity(intent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && data != null) {
+            // Verifica se ci sono più immagini
+            val imagesUri = data.clipData
+            arrayOfBitmap.clear()//Pulisco l'array ogni volta che acquisico cose nuove(Sarebbe meglio farlo dopo aver creato il pdf)
+            if (imagesUri != null) {
+                // Se sono state selezionate più immagini
+                for (i in 0 until imagesUri.itemCount) {
+                    val imageUri = imagesUri.getItemAt(i).uri
+                    val stream = contentResolver.openInputStream(imageUri)
+                    arrayOfBitmap.add(BitmapFactory.decodeStream(stream))
+                }
+            } else {
+                // Se c'è una sola immagine
+                val imageUri = data.data
+                if (imageUri != null) {
+                    val stream = contentResolver.openInputStream(imageUri)
+                    arrayOfBitmap.add(BitmapFactory.decodeStream(stream))
+                }
+            }
+            Toast.makeText(baseContext, "$imagesUri.itemCount", Toast.LENGTH_SHORT).show()
+        }
     }
 }
