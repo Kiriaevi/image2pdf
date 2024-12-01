ad97034fc85628a14b61d8357a3f2e3eb57a5001
Francesco <91418890+Kiriaevi@users.noreply.github.com>
2024-12-01 14:19:15 +0100

    Aggiunta accozzaglia di base per generare PDF in remoto da terminale su linux

--- app/src/main/java/com/example/image2pdf/linuxIntegration/LinuxIntegration.kt
+++ app/src/main/java/com/example/image2pdf/linuxIntegration/LinuxIntegration.kt
@@ -6,22 +6,41 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.image2pdf.R
+import fi.iki.elonen.NanoHTTPD.SOCKET_READ_TIMEOUT
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class linuxIntegration : AppCompatActivity() {
     companion object {
         private val TAG = "LINUXINT"
         private val porta = 8080
     }
+    private var serverExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+    private var server: ServerNanoHttpd? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_linux_integration)
-
         try {
-            avviaServer()
+            serverExecutor.execute{avviaServer()}
         } catch(exc: Exception) {
             Toast.makeText(this, "Si è verificato un errore nella creazione del server", Toast.LENGTH_LONG).show()
             Log.e(TAG, "Si è verificato un errore nell'avvio del server ${exc}", exc)
         }
     }
+
+    private fun avviaServer() {
+        server = ServerNanoHttpd(porta)
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        server?.let{
+            server!!.stop()
+        } ?: run {
+            Log.e(TAG, "Il server è nullo, non è stato mai avviato!")
+            throw NullPointerException("Il server è nullo, non è stato mai avviato!")
+        }
+    }
+
 }
