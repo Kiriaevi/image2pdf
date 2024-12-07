e3d3be053f49921bd63cdb58a843c3ba5d1cd7e8
Francesco <91418890+Kiriaevi@users.noreply.github.com>
2024-12-07 18:38:54 +0100

    Aggiunto bottone per abilitare e disabilitare il server;

--- app/src/main/java/com/example/image2pdf/linuxIntegration/LinuxIntegration.kt
+++ app/src/main/java/com/example/image2pdf/linuxIntegration/LinuxIntegration.kt
@@ -2,15 +2,16 @@ package com.example.image2pdf.linuxIntegration
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageButton
+import android.widget.CompoundButton
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.image2pdf.R
-import fi.iki.elonen.NanoHTTPD.SOCKET_READ_TIMEOUT
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+
 
 class linuxIntegration : AppCompatActivity() {
     companion object {
@@ -25,28 +26,43 @@ class linuxIntegration : AppCompatActivity() {
         setContentView(R.layout.activity_linux_integration)
 
         val bottoneStampa = findViewById<TextView>(R.id.comando)
+        impostaComportamentoSwitch()
         bottoneStampa.setText("curl -X POST -F file1=@/path/file/1 -F file2=@/path/file/2 --output nomeFileOutput.pdf http://${ServerNanoHttpd.ipLocale()}:8080")
+
+    }
+
+    private fun impostaComportamentoSwitch() {
+        val switchServer = findViewById<Switch>(R.id.switch1)
+        switchServer.setTextOn("Attivato")
+        switchServer.setTextOff("Disattivato")
+        switchServer.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                avviaServer()
+            } else {
+                disattivaServer()
+            }
+        }
+    }
+    private fun avviaServer() {
         try {
             Log.e(TAG, "IP LOCALE: ${ServerNanoHttpd.ipLocale()}")
-            serverExecutor.execute{avviaServer()}
+            serverExecutor.execute{server = ServerNanoHttpd(porta)}
         } catch(exc: Exception) {
             Toast.makeText(this, "Si è verificato un errore nella creazione del server", Toast.LENGTH_LONG).show()
             Log.e(TAG, "Si è verificato un errore nell'avvio del server ${exc}", exc)
         }
     }
-
-    private fun avviaServer() {
-        server = ServerNanoHttpd(porta)
-
-    }
-    override fun onDestroy() {
-        super.onDestroy()
+    private fun disattivaServer() {
         server?.let{
             server!!.stop()
         } ?: run {
             Log.e(TAG, "Il server è nullo, non è stato mai avviato!")
             throw NullPointerException("Il server è nullo, non è stato mai avviato!")
         }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        disattivaServer()
     }
 
 }
